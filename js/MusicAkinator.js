a865d176544bb813d1966a6c092096169d12538a
heletrix <havrilyuk.olena@gmail.com>
2020-01-24 04:04:15 +0200

    added result lists and input check

--- js/MusicAkinator.js
+++ js/MusicAkinator.js
@@ -4,7 +4,9 @@ let attemptNumber = 0;
 let gameNumber = 1;
 let scoreAkinator = 0;
 let scoreUser = 0;
-let results = []
+let results = [];
+let suggestedSongs = [];
+let textRequests = [];
 
 $(document).ready(function(){
 	$("#totalAttemptNumber").text(totalAttempt);
@@ -12,27 +14,47 @@ $(document).ready(function(){
 
 	$('#inputSongTextForm').submit(function() {
 		// ToDo: add spiner for request executing
-		// toDo: validatin of empty string in input
-		let textSong = $('#songTextInput').val();
-		$.ajax({
-			url: host + '/api/audio/search',
-			type: 'GET',
-			contentType: "application/json",
-			data: {
-				songText: textSong
-			},
-			crossDomain: true,		
-			success: function (result) {
-				// Todo: to add error handling
-				results.push(result['result']);
-				showGuessForm();
-			},
-			error: function (xhr, ajaxOptions, thrownError) {
-				errorAlert(xhr, thrownError);
-			}
-		});
+		let textSong = $('#songTextInput').val().trim();
+		if (textSong === '') { 
+			$('#errorInput').text('Please, enter song text!');
+		} else {
+			$('#errorInput').text('');
+			textRequests.push(textSong);
+			$.ajax({
+				url: host + '/api/audio/search',
+				type: 'GET',
+				contentType: "application/json",
+				data: {
+					songText: textSong
+				},
+				crossDomain: true,		
+				success: function (result, textStatus, xhr) {
+
+					if (xhr.status === 200) {
+						results.push(result['result']);
+						suggestedSongs.push([]);
+						showGuessForm();
+					} else if (xhr.status === 204) {
+						results.push({comment: 'No ideas'});
+						scoreUser++;
+						suggestedSongs.push([]);
+						$('#whoIsWinner').text(`You win! I have any ideas`);
+						showRoundResultForm();
+					} else {
+						$('#gameAdditionalText').text('Ups... Something went wrong, I must finish the game');
+						showGameResult();
+						console.log('error');
+					}
+					// Todo: to check error handling
+				},
+				error: function (xhr, ajaxOptions, thrownError) {
+					errorAlert(xhr, thrownError);
+				}
+			});
+		}
+		$('#songTextInput').val('');
 		event.preventDefault();
-	  });
+	  });		
 });
 
 function errorAlert(xhr, thrownError) {
@@ -45,23 +67,22 @@ function errorAlert(xhr, thrownError) {
 
 function showGuessForm() {
 	if(results[gameNumber-1].length > attemptNumber) {
+		suggestedSongs[gameNumber-1].push(results[gameNumber-1][attemptNumber]);
+		console.log(suggestedSongs);
 		$('#songAuthor').text(results[gameNumber-1][attemptNumber].artist);
 		$('#songTitle').text(results[gameNumber-1][attemptNumber].title);
 		$('#audioUrl').attr("src", results[gameNumber-1][attemptNumber].previewLink);
 		attemptNumber++;
-		$('#inputSongTextForm, #raundResult, #gameResult').hide();
+		$('#inputSongTextForm, #roundResult, #gameResult').hide();
 		$('#suggestedSong').show();
 	} else {
-		// ToDo: add logic for less number of attempts
+		// ToDo: check logic for less number of attempts
 		scoreUser++;
-		$('#raundAdditionalText').text(`I have no ideas, so you won this raund`);
-		showRaundResultForm();
-		
-		
+		$('#whoIsWinner').text(`You win this round! I have no more ideas`);
+		showRoundResultForm();
 	}
 	updateScoreHTML();
 	resetAudio();
-	
 }
 
 function updateScoreHTML(){
@@ -73,41 +94,89 @@ function updateScoreHTML(){
 
 // pressed Yes
 function songGuessed() {
+	$('#whoIsWinner').text('You win this round!');
 	scoreAkinator++;
-	showRaundResultForm();
+	console.log(gameNumber-1, attemptNumber-1)
+	suggestedSongs[gameNumber-1][attemptNumber-1].isTrueSong = true;
+	showRoundResultForm();
 	resetAudio();
 }
 
 // pressed No
 function nextAttempt() {
+	console.log(gameNumber-1, attemptNumber-1)
+	suggestedSongs[gameNumber-1][attemptNumber-1].isTrueSong = false;
 	if (attemptNumber < totalAttempt) {
 		showGuessForm();
 	} else {
+		$('#whoIsWinner').text('I win this round!');
 		scoreUser++;
-		showRaundResultForm();
+		showRoundResultForm();
 	}
 	resetAudio();
 }
 
-function showRaundResultForm() {
+function showRoundResultForm() {
 	updateScoreHTML();
+	let roundHtml = '';
+	console.log(suggestedSongs);
+	if (suggestedSongs[gameNumber-1].length !== 0) {
+		roundHtml += '<ol>';
+		for(let i = 0; i < suggestedSongs[gameNumber-1].length; i++) {
+			roundHtml += `<li>${suggestedSongs[gameNumber-1][i].artist} - ${suggestedSongs[gameNumber-1][i].title}</li>`;
+		}
+		roundHtml += '</ol>';
+	} else {
+		roundHtml += '<p>No songs</p>';
+	}
+	console.log(roundHtml);
+	$('#roundSongList').html(roundHtml);
 	$('#inputSongTextForm, #suggestedSong, #gameResult').hide();
-	$('#raundResult').show(); 
-
+	$('#roundResult').show(); 
+	$('#songTextInput').val("");
 }
 
 function showGameResult() {
+	let gameHtml = '<ol>';
+	for (let j = 0; j < suggestedSongs.length; j++) {
+		gameHtml += '<li>';
+		if (suggestedSongs[j].length === 0) {
+			gameHtml += `Round ${j+1}: no songs`;
+		} else {
+			gameHtml += `Round ${j+1}:<ol>`;
+			if (suggestedSongs[j]) {
+				for(let i = 0; i < suggestedSongs[j].length; i++) {
+					gameHtml += `<li>${suggestedSongs[j][i].artist} - ${suggestedSongs[j][i].title}</li>`;
+				}
+			} else {
+				gameHtml += 'no songs';
+			}
+				gameHtml += '</ol>';
+		}
+		gameHtml += '</li>';
+	}
+	gameHtml += '</ol>';
+	$('#gameSongList').html(gameHtml);
+
+	if (scoreAkinator < scoreUser) {
+		$('#gameAdditionalText').text(`You are winner! Your score is ${scoreUser}`);
+	} else if (scoreAkinator > scoreUser) {
+		$('#gameAdditionalText').text(`Akinator is winner! His score is ${scoreAkinator}`);
+	} else {
+		$('#gameAdditionalText').text(`Friendship won the game!`);
+	}
+
 	$('#songTextInput').val("");
-	$('#inputSongTextForm, #suggestedSong, #raundResult').hide();
+	$('#inputSongTextForm, #suggestedSong, #roundResult').hide();
 	$('#gameResult').show(); 
 }
 
-function newRaund() {
+function newRound() {
 	attemptNumber = 0;
 	gameNumber++;
 	updateScoreHTML();
-	$('#raundAdditionalText').text('');
-	$('#suggestedSong, #raundResult, #gameResult').hide();
+	$('#roundAdditionalText').text('');
+	$('#suggestedSong, #roundResult, #gameResult').hide();
 	$('#inputSongTextForm').show(); 
 }
 
@@ -116,13 +185,16 @@ function resetGameValues() {
 	gameNumber = 1;
 	scoreAkinator = 0;
 	scoreUser = 0;
-	results = []
+	results = [];
+	suggestedSongs = [];
+	textRequests = [];
+	$('#gameAdditionalText').text('');
 }
 
 function newGame() {
 	resetGameValues();
 	updateScoreHTML();
-	$('#suggestedSong, #raundResult, #gameResult').hide();
+	$('#suggestedSong, #roundResult, #gameResult').hide();
 	$('#inputSongTextForm').show(); 
 }
 
